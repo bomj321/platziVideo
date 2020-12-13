9795e49eaed0bf4322ad05d8436fd7f699c23b8f
bomj321 <jmob612@gmail.com>
2020-12-13 05:05:49 -0400

    esLint

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./components/HelloWorld";
+
+const container = document.getElementById("app");
+
+ReactDOM.render(<HelloWorld />, container);
